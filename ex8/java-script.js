68158d22eb593e6075d3839674d8e2a9917da403
Delia Davidescu <deliaalina.davidescu@gmail.com>
2018-09-13 10:54:55 +0200

    Ex8

---
+++ ex8/java-script.js
@@ -0,0 +1,24 @@
+var a = 7;
+
+var b = 14;
+
+var c = "21";
+
+var d = '36';
+
+var e = 42;
+
+var sum=a+ b + Number(c) + Number(d) + e;
+document.write("Sum result is: " + sum + "</br>");
+
+var f = '1';
+
+var g = 15;
+
+var h = 8;
+
+var i = "1";
+
+var mult = Number(f) * g * h * Number(i); 
+document.write("Multiply result is: " + mult + "</br>");
+document.write("Division result is: " + sum/mult);
