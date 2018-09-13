a9c34f71749baac225335fcb273909349255ff9d
mustafa920 <tafocosmos@gmail.com>
2018-09-13 10:21:55 +0200

    ex6

---
+++ ex6/java-script.js
@@ -0,0 +1,10 @@
+var start = ["apple" , "banana" , "kiwi"] ;
+start.push("orange") ;
+console.log(start);
+start.pop();
+console.log(start);
+
+var start = ["monkey" , "horse" , "dog"] ;
+console.log(start.sort());
+start.unshift("cat");
+console.log(start);
