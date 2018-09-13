b4693857224fede2fe8cbe7ac568908507a02b04
Delia Davidescu <deliaalina.davidescu@gmail.com>
2018-09-13 12:02:44 +0200

    ex10

---
+++ Ex10/java-script.js
@@ -0,0 +1,4 @@
+var city=[["Vienna"],[0,"is"],[0,0,"a"],[0,0,0, "nice"], [0,0, 0,0, "city"]];
+document.write(city[0][0] + " " + city[1][1] + " " + city[2][2] + " " + city[3][3] + " " + city[4][4] + ".");
+
+
--- ex8/java-script.js
+++ ex8/java-script.js
@@ -1,4 +1,4 @@
-var a = 7;
+/*var a = 7;
 
 var b = 14;
 
@@ -21,4 +21,8 @@ var i = "1";
 
 var mult = Number(f) * g * h * Number(i); 
 document.write("Multiply result is: " + mult + "</br>");
-document.write("Division result is: " + sum/mult);
+document.write("Division result is: " + sum/mult);*/
+
+var a="Properties,$a$set$of$immutable$values,$are$passed$to$a$component\'s$renderer$as$properties$in$its $HTML$tag.$A$component$cannot$directly$modify$any$properties$passed$to$it,$but$can$be$passed$ callback$functions$that$do$modify$values.$This$mechanism\'s$promise$is$expressed$as$properties $flow$down;$actions$flow$up."
+
+console.log(a.replace(/\$/g, ' '))
